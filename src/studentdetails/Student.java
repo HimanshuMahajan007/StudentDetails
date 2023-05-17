baa8c0b7d7982844c5707ba4d1b7bea6efec1e87
HimanshuMahajan007 <mahajahi@sheridancollege.ca>
2023-05-17 17:03:13 -0400

    student version 1

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,49 @@
+package studentdetails;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * Modelling the student class
+ * @author himanshu
+ */
+public class Student {
+    private String name;        // Encapsulation
+    private int age;
+    
+    // Defining constructor to set values for the local variables
+    public Student (String na, int ag) {
+        this.name = na;
+        this.age = ag;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdetails;
+import java.util.Scanner;
+
+/**
+ *
+ * @author himanshu
+ */
+public class StudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Array of object declaration
+//        Student[] list = new Student[2];
+//        Student s1 = new Student();
+//        s1.setName("Himanshu");
+//        s1.setAge(19);
+//        Student s2 = new Student();
+//        s2.setName("Nandini");
+//        s2.setAge(16);
+//        list[0] = s1;
+//        list[1] = s2;
+//        System.out.println(list[0].getName() + " " + list[0].getAge());
+
+
+        // Example of array of objects: -
+        Student[] list = new Student[3];
+        Scanner keyboard = new Scanner(System.in);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print("Write your name: ");
+            String n = keyboard.next();
+            System.out.print("Write your age: ");
+            int a = keyboard.nextInt();
+            Student s1 = new Student(n, a);
+            list[i] = s1;
+        }
+        // Read the data i stored in array list
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i].getName() + " " + list[i].getAge());
+        }
+    }
+    
+}
